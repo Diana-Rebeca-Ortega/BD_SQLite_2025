c98f2548138e441e48cf413e0e018801c2390299
Diana Rebeca Ortega <diana.os@tecjerez>
2025-11-27 18:24:40 -0600

    Se subio el proyecto al repositorio

---
+++ app/src/main/java/com/example/bd_sqlite_2025/ActivityAltas.java
@@ -0,0 +1,17 @@
+package com.example.bd_sqlite_2025;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+//paso 1
+public class ActivityAltas  extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_altas);
+
+
+
+    }
+}
